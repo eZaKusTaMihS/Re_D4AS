fcacbbce9cefb7f327f043606666f4f7b6dd0195
eZaKaMihS <hzh200412@gmail.com>
2024-12-04 02:19:41 +0800

    correct parser

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from d4 import GameController
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--serial', '-s', type=str, default='', nargs=1, required=False)
-    parser.add_argument('--window', '-w', type=str, default='', nargs=1, required=False)
+    parser.add_argument('--serial', '-s', type=str, default='', required=False)
+    parser.add_argument('--window', '-w', type=str, default='', required=False)
     args = parser.parse_args()
     config = json.load(open('config.json'))
     controller = GameController(config, args)
