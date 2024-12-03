5b6c0e9175f49a8106f6d8f9fc5d71187efbd47f
eZaKaMihS <hzh200412@gmail.com>
2024-12-04 02:26:23 +0800

    add default args

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from d4 import GameController
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--serial', '-s', type=str, default='', required=False)
-    parser.add_argument('--window', '-w', type=str, default='', required=False)
+    parser.add_argument('--serial', '-s', type=str, default='127.0.0.1:16416', required=False)
+    parser.add_argument('--window', '-w', type=str, default='d4dj', required=False)
     args = parser.parse_args()
     config = json.load(open('config.json'))
     controller = GameController(config, args)
