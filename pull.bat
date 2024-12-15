fd1bd935725ece272f10da46980df46f39a282ed
eZaKaMihS <hzh200412@gmail.com>
2024-12-16 04:32:17 +0800

    pull

--- pull.bat
+++ pull.bat
@@ -1,5 +1,2 @@
-git update-index --skip-worktree config.json
-git fetch --all
-git reset --hard origin/master
-git pull
+git update-index --skip-worktree config.json && git fetch --all && git reset --hard origin/master && git pull
 @pause
