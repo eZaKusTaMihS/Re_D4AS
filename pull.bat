3a056e3431c370f082d3a6bb8c8328a2250ad21f
eZaKaMihS <hzh200412@gmail.com>
2024-12-16 04:57:38 +0800

    pull

--- pull.bat
+++ pull.bat
@@ -1,5 +1,2 @@
-git update-index --skip-worktree config.json, res/stat/live/ded.png
-git fetch --all
-git reset --hard origin/master
-git pull
+git update-index --skip-worktree config.json res/stat/live/ded.png&&git fetch --all&&git reset --hard origin/master&&git pull
 @pause
